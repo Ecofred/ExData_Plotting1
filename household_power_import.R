f2b8e1795663cfbd1f48b3d3ade76bfdcabcd620
Frederic Clement <f.clems@gmail.com>
2019-01-02 22:45:58 +0100

    retrieve the data in the working directory

--- household_power_import.R
+++ household_power_import.R
@@ -2,13 +2,22 @@ library(lubridate)
 library(dplyr)
 library(readr)
 
+# GETTING THE FILE
 house_power_file <- 
-  "exdata_data_household_power_consumption/household_power_consumption.txt"
+  "household_power_consumption"
+house_power_url <-
+  'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(url = 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip',
+              destfile = paste0(house_power_file,'.zip'))
+
+unzip(zipfile = paste0(house_power_file,'.zip'),
+      exdir = getwd()) 
 
 
+# LOAD + SUBSET + FORMAT DATETIME
 house_power <-
   read_delim(
-    house_power_file,
+    paste0(house_power_file, '.txt'),
     delim = ';',
     col_types = cols('c', 'c', 'd', 'd', 'd', 'd', 'd', 'd', 'd'),
     na = '?'
